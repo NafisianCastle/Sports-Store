4c9b56b236c145bdfac7860d58c09c36116203f4
NafisianCastle <nafis.cseaiub@gmail.com>
2022-07-23 01:14:02 +0600

    product delete feature

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -24,8 +24,8 @@ namespace SportsStore.Models
         [Required(ErrorMessage = "Please enter a country name")]
         public string Country { get; set; }
         public bool GiftWrap { get; set; }
-       
-        [BindNever] 
+
+        [BindNever]
         public bool Shipped { get; set; }
 
     }
